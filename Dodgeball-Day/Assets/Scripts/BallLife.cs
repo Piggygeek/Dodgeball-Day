4568919ff50c6c22fa27ae053a334b094c7bcedf
Chaos828 <lenahanr8282000@gmail.com>
2019-01-10 13:15:26 -0800

    code

---
+++ Dodgeball-Day/Assets/Scripts/BallLife.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLife : MonoBehaviour {
+
+	public float m_MaxLifeTime = 2f;                    // The time in seconds before the shell is removed.
+	public Rigidbody dodgeBall;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// If it isn't destroyed by then, destroy the shell after it's lifetime.
+		Destroy (dodgeBall, m_MaxLifeTime);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
