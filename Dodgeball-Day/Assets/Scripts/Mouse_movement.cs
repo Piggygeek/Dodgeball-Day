de5ec20235746da82a0464dda06b0a61fdbe5ef7
Piggygeek <piggygeek@gmail.com>
2019-01-11 12:51:53 -0800

    hope

--- Dodgeball-Day/Assets/Scripts/Mouse_movement.cs
+++ Dodgeball-Day/Assets/Scripts/Mouse_movement.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mouse_movement : MonoBehaviour {
+public class Mouse_movement : MonoBehaviour 
+{
+	float horizontalSpeed = 2.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		// Get the mouse delta. This is not in the range -1...1
+		float h = horizontalSpeed * Input.GetAxis("Mouse X");
+
+		transform.Rotate(0, h, 0);
 	}
 }
