963bc7b2e37346a12d770e30e41a9e625af18123
Piggygeek <piggygeek@gmail.com>
2019-01-11 13:02:53 -0800

    Player

---
+++ Dodgeball-Day/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour 
+	{
+		float horizontalSpeed = 2.0f;
+		
+
+		void Update()
+		{
+			// Get the mouse delta. This is not in the range -1...1
+			float h = horizontalSpeed * Input.GetAxis("Mouse X");
+			
+
+			transform.Rotate(0, h, 0);
+		}
+	}
