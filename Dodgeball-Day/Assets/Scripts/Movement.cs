c572c0c587a85e70c02c868fb43416188ac90e4b
Piggygeek <piggygeek@gmail.com>
2019-01-14 12:58:10 -0800

    left and right movement

--- Dodgeball-Day/Assets/Scripts/Movement.cs
+++ Dodgeball-Day/Assets/Scripts/Movement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Movement : MonoBehaviour 
 {
 	private Rigidbody m_Rigidbody;
-	private float m_MovementInputValue;
-	private string m_MovementAxisName;
+	private float m_MovementInputValueFB;
+	private float m_MovementInputValueLR;
+	private string m_MovementAxisNameFB;
+	private string m_MovementAxisNameLR;
 	public float m_Speed = 12f; 
 
 	private void Awake ()
@@ -16,26 +18,37 @@ public class Movement : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		m_MovementInputValue = 0f;
-		m_MovementAxisName = "Vertical";
+		m_MovementInputValueFB = 0f;
+		m_MovementInputValueLR = 0f;
+		m_MovementAxisNameFB = "Vertical";
+		m_MovementAxisNameLR = "Horizontal";
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
+		m_MovementInputValueFB = Input.GetAxis (m_MovementAxisNameFB);
+		m_MovementInputValueLR = Input.GetAxis (m_MovementAxisNameLR);
 	}
 	private void FixedUpdate ()
 	{
 		// Adjust the rigidbodies position and orientation in FixedUpdate.
-		Move ();
+		MoveFB ();
+		MoveLR ();
 	}
-	private void Move ()
+	private void MoveFB ()
 	{
 		// Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
-		Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+		Vector3 movementFB = transform.forward * m_MovementInputValueFB * m_Speed * Time.deltaTime;
 
 		// Apply this movement to the rigidbody's position.
-		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+		m_Rigidbody.MovePosition(m_Rigidbody.position + movementFB);
+
+	}
+	private void MoveLR ()
+	{
+		Vector3 movementLR = transform.right * m_MovementInputValueLR * m_Speed * Time.deltaTime;
+
+		m_Rigidbody.MovePosition (m_Rigidbody.position + movementLR);
 	}
 }
