b6c41ac7d8c3dd1754f44a8bc579de0448441046
Penzy <cdvsdca@gmail.com>
2019-01-16 13:20:24 -0800

    movement of boussssssssssssssssssssssssssssssssssssssssssssssss

---
+++ Dodgeball-Day/Assets/Scripts/Boss.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+	public float min=5f;
+	public float max=10f;
+	// Use this for initialization
+	void Start () {
+
+		min=transform.position.x;
+		max=transform.position.x+15;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		transform.position =new Vector3(Mathf.PingPong(Time.time*10,max-min)+min, transform.position.y, transform.position.z);
+
+	}
+}
