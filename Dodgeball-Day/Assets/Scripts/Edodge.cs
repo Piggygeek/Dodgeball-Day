18ec68128ae17358a7dfec86361a9d16a3e75fb5
Chaos828 <lenahanr8282000@gmail.com>
2019-01-17 13:31:48 -0800

    code

---
+++ Dodgeball-Day/Assets/Scripts/Edodge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edodge : MonoBehaviour {
+
+	public float speed = 5f;
+	public Rigidbody EdodgeBall;
+	private float m_CurrentLaunchForce = 25f;
+	public Transform EnemyShoot;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		EdodgeBall.velocity = m_CurrentLaunchForce * EnemyShoot.forward; ;
+	}
+
+}
