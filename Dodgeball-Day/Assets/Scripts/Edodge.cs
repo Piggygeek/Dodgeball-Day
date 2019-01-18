b4a5ce552fb56fed4e7d92a430df3055ed6a9f38
Chaos828 <lenahanr8282000@gmail.com>
2019-01-18 12:36:58 -0800

    code

--- Dodgeball-Day/Assets/Scripts/Edodge.cs
+++ Dodgeball-Day/Assets/Scripts/Edodge.cs
@@ -8,7 +8,14 @@ public class Edodge : MonoBehaviour {
 	public Rigidbody EdodgeBall;
 	private float m_CurrentLaunchForce = 25f;
 	public Transform EnemyShoot;
+	public Transform Target;
 
+	public float range = 70f;
+	public string PlayerTag = "Player";
+
+	public Transform partToRotate2;
+	public float fireRate = 2f;
+	private float fireCountdown;
 
 
 	// Use this for initialization
@@ -24,6 +31,38 @@ public class Edodge : MonoBehaviour {
 	void Update ()
 	{
 		EdodgeBall.velocity = m_CurrentLaunchForce * EnemyShoot.forward; ;
+
+		if (Target == null)
+			return;
+
+		Vector3 dir = Target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation (dir);
+		Vector3 rotation = lookRotation.eulerAngles;
+		partToRotate2.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+
+	}
+
+	void UpdateTarget ()
+	{
+		GameObject[] player = GameObject.FindGameObjectsWithTag ("Player");
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+		foreach (GameObject Player in player) 
+		{
+			float distanceToEnemy = Vector3.Distance (transform.position, Player.transform.position);
+			if (distanceToEnemy < shortestDistance) 
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = Player;
+			}
+		}
+		if (nearestEnemy != null && shortestDistance <= range) {
+			Target = nearestEnemy.transform;
+		} 
+		else 
+		{
+			Target = null;
+		}
 	}
 
 }
